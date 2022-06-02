205e76f1974337821b6a05929248a80ffefb6246
wawababa <walwaalemayehu@gmail.com>
2022-06-02 14:17:18 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 /**
- * main - prints a string
- *
- * Return: Returns zero
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-        printf("\"with proper grammar, but the outcome is a piece of art,");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
