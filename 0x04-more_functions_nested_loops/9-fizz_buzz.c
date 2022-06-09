6c62c3a133b0ee60940fd8764904987ef67ff9ec
wawababa <walwaalemayehu@gmail.com>
2022-06-09 11:24:40 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,46 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_triangle - prints triangle
- * @size: print size
- * Return: Always 0.
+ * main - Entry piont
+ * Return: Always 0
  */
-
-void print_triangle(int size)
-
+int main(void)
 {
+	int i = 1;
 
+	for (; i < 100 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
 
-int i;
-int z;
-int d;
-int p;
-
-if (size > 0)
-{
-d = size - 1;
-for (i = 0; i < size ; i++)
-{
-for (z = d; z > 0 ; z--)
-{
-_putchar (' ');
-}
-
-for (p = 0; p <= i; p++)
-{
-_putchar (35);
-}
-
-d--;
-
-_putchar ('\n');
-
-
-}
-
-}
-
-else
-{
-_putchar ('\n');
-}
+	return (0);
 }
