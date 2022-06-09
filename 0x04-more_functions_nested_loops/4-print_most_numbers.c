3e6c16970b115e2fc4144b0e5b57e0d6e4eb8d0c
wawababa <walwaalemayehu@gmail.com>
2022-06-09 11:06:25 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,19 @@
 #include "main.h"
+
 /**
- * print_most_numbers - print numbers
+ * print_most_numbers - prints the numbers
  *
- * Return: Always 0.
  */
 void print_most_numbers(void)
 {
+	int i;
 
-int i;
-
-i = 0;
-while (i < 10)
-{
-if (i != 2 && i != 4)
-{
-_putchar (i + '0');
-}
-i++;
-}
-
-_putchar ('\n');
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' &&  i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
 }
