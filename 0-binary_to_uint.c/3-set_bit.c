ac95e03ab864dd3e17fb8ab69a05f3ae2190e8bc
wawababa <walwaalemayehu@gmail.com>
2022-07-22 04:00:51 -0700

    all task

--- 0-binary_to_uint.c/3-set_bit.c
+++ 0-binary_to_uint.c/3-set_bit.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * set_bit - sets value of a bit to 1 at a given index
- * @n: decimal number passed by pointer
- * @index: index position to change, starting from 0
- * Return: 1 if it worked, -1 if error
+ * set_bit - sets the value of a bit to 1.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int setnum = 1;
+	unsigned int m;
 
-	setnum <<= index;
-
-	if (index > sizeof(unsigned long int) * 8)
+	if (index > 63)
 		return (-1);
 
-	(*n) = (*n) | setnum;
+	m = 1 << index;
+	*n = (*n | m);
+
 	return (1);
 }
--- 0-binary_to_uint.c/4-clear_bit.c
+++ 0-binary_to_uint.c/4-clear_bit.c
@@ -1,18 +1,24 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * clear_bit - set the value of a bit to 0 at a given index
- * @n: pointer to decimal number to change
- * @index: index position to change
- * Return: 1 if it worked, -1 if error
+ * clear_bit - sets the value of a bit to 0.
+ * at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int setnum = 1;
+	unsigned int m;
 
-	setnum <<= index;
-	if (index > sizeof(unsigned long int) * 8)
+	if (index > 63)
 		return (-1);
-	(*n) = (*n) & ~(setnum);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
 	return (1);
 }
