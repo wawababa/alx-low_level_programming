c9411ddf2a6238ce583f271908ac0263478af319
wawababa <walwaalemayehu@gmail.com>
2023-01-16 01:26:40 -0800

    all task

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,31 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+
